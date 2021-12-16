97b14d77cdba3dd1b2b9a5fc530ef633eb8940bd
Ton-lee <51439666+Ton-lee@users.noreply.github.com>
2021-12-16 12:42:26 +0800

    error calculation

--- error_test.m
+++ error_test.m
@@ -1,3 +1,4 @@
+% error testing function. We calculate the F-norm error as well as the relative error
 function [error,error_rate] = error_test(A,index_num) 
 % to compute the error caused by the approximation
 % input : the initial matrix A & the choice of column index index_num
